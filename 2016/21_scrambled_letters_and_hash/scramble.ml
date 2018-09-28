c60cd4d7997abe4a6b7a8d4bb00cdcb6480ad20e
Dean Merchant <dean@piratestudios.co.uk>
2018-09-28 19:05:21 +0100

    21 WIP

--- 2016/21_scrambled_letters_and_hash/scramble.ml
+++ 2016/21_scrambled_letters_and_hash/scramble.ml
@@ -25,7 +25,7 @@ module Parser = struct
   let reverse_p = reverse <$> (string "reverse positions " *> integer) <*> (string " through " *> integer)
   let move_p = move <$> (string "move position " *> integer) <*> (string " to position " *> integer) 
 
-  let operation = many (char ' ') *> (
+  let operation = (
       swap_positions <|>
       swap_letters <|>
       rotate_steps <|>
@@ -53,9 +53,7 @@ module Parser = struct
     |> [%sexp_of : operation list]
     |> print_s;
     [%expect {|
-      ((SwapPosition 4 0) (SwapLetter d b) (Reverse 0 4) (RotateOnIndex Left 1)
-       (Move 1 4) (Move 3 0) (RotateOnChar b) (RotateOnChar d)) |}]
-
+      ((SwapPosition 4 0)) |}]
 end
 
 let rec apply str = function
@@ -71,125 +69,99 @@ let rec apply str = function
   let (b_idx, _) = Array.findi_exn str ~f:(fun _ chr -> chr = b) in
   apply str (SwapPosition (a_idx, b_idx))  
 )
-| RotateSteps (_, _) -> str
+| RotateSteps (Right, steps) -> if (steps = 0) then str else (
+  let n = steps % Array.length str in
+  let len = Array.length str in
+  apply 
+    (Array.append [|Array.get str (len - 1)|] (Array.slice str 0 (len - 1)))
+    (RotateSteps (Right, n - 1))
+)
+| RotateSteps (Left, steps) -> if (steps = 0) then str else (
+  let n = steps % Array.length str in
+  let len = Array.length str in
+  apply 
+    (Array.append (Array.slice str 1 len) [|Array.get str 0|])
+    (RotateSteps (Left, n - 1))
+)
 | RotateOnChar chr_val -> (
     let (chr_idx, _) = Array.findi_exn str ~f:(fun _ chr -> chr = chr_val) in
-    apply str (RotateSteps (Right, chr_idx))
+    let rotations = 1 + chr_idx + (if chr_idx >= 4 then 1 else 0) in
+    apply str (RotateSteps (Right, rotations))
 )
-| Reverse (start, fin) -> str
-| Move (origin, destingation) -> (
+| Reverse (start, fin) -> (
+  let pre = if start = 0 then [||] else Array.slice str 0 start in
+  let mid = Array.slice str start (fin + 1) in
+  let post = Array.slice str (fin + 1) (Array.length str) in
+  Array.rev_inplace mid;
+  Array.concat [pre; mid; post]
+)
+| Move (origin, dest) -> (
+  let len = Array.length str in
   let orig_val = Array.get str origin in
-  let pre = Array.slice str 0 origin in
-  let post = Array.slice str origin (Array.length str) in
-  str
+
+  let pre = if origin = 0 then [||] else Array.slice str 0 origin in
+  let post = Array.slice str (origin + 1) len in
+  let without_orig = Array.append pre post in
+  let without_len = Array.length without_orig in
+
+  let pre_with = if dest = 0 then [||] else Array.slice without_orig 0 dest in 
+  let post_with = if dest >= without_len then [||] else
+    Array.slice without_orig dest (Array.length without_orig) in 
+  Array.concat [pre_with;[|orig_val|];post_with]
 )
 
 let scrable str operations = List.fold_left operations ~init:str ~f:apply
 
+let%test_unit _ =
+  [%test_result : char array] 
+    (scrable (String.to_array "abcde") [SwapPosition (4,0)])
+    ~expect:[|'e';'b';'c';'d';'a'|]
+
+let%test_unit _ =
+  [%test_result : char array] 
+    (scrable (String.to_array "ebcda") [SwapLetter ('d','b')])
+    ~expect:[|'e';'d';'c';'b';'a'|]
+
+let%test_unit _ =
+  [%test_result : char array] 
+    (scrable (String.to_array "edcba") [Reverse (0, 4)])
+    ~expect:[|'a';'b';'c';'d';'e'|]
+
+let%test_unit _ =
+  [%test_result : char array] 
+    (scrable (String.to_array "abcde") [RotateSteps (Left, 1)])
+    ~expect:[|'b';'c';'d';'e';'a'|]
+
+let%test_unit _ =
+  [%test_result : char array] 
+    (scrable (String.to_array "bcdea") [Move (1, 4)])
+    ~expect:[|'b';'d';'e';'a';'c'|]
+
+let%test_unit _ =
+  [%test_result : char array] 
+    (scrable (String.to_array "bdeac") [Move (3, 0)])
+    ~expect:[|'a';'b';'d';'e';'c'|]
+
+let%test_unit _ =
+  [%test_result : char array] 
+    (scrable (String.to_array "abdec") [RotateOnChar 'b'])
+    ~expect:[|'e';'c';'a';'b';'d'|]
+
+let%test_unit _ =
+  [%test_result : char array] 
+    (scrable (String.to_array "ecabd") [RotateOnChar 'd'])
+    ~expect:[|'d';'e';'c';'a';'b'|]
+
 let%expect_test _ =
   (In_channel.read_all "./statements.txt") 
   |> Parser.parse
-  |> scrable (String.to_array "abcdefgh")
-  |> [%sexp_of : char array]
+  (* |> scrable (String.to_array "abcdefgh") *)
+  |> [%sexp_of : operation list]
+  (* |> [%sexp_of : operation list] *)
+  (* |> [%sexp_of : string] *)
   |> print_s;
   [%expect {|
-    rotate right 1 step
-    swap position 2 with position 4
-    rotate based on position of letter g
-    rotate left 4 steps
-    swap position 6 with position 0
-    swap letter h with letter a
-    swap letter d with letter c
-    reverse positions 2 through 4
-    swap position 2 with position 4
-    swap letter d with letter e
-    reverse positions 1 through 5
-    swap letter b with letter a
-    rotate right 0 steps
-    swap position 7 with position 3
-    move position 2 to position 1
-    reverse positions 2 through 5
-    reverse positions 4 through 7
-    reverse positions 2 through 7
-    swap letter e with letter c
-    swap position 1 with position 7
-    swap position 5 with position 7
-    move position 3 to position 6
-    swap position 7 with position 2
-    move position 0 to position 7
-    swap position 3 with position 7
-    reverse positions 3 through 6
-    move position 0 to position 5
-    swap letter h with letter c
-    reverse positions 2 through 3
-    swap position 2 with position 3
-    move position 4 to position 0
-    rotate based on position of letter g
-    rotate based on position of letter g
-    reverse positions 0 through 2
-    swap letter e with letter d
-    reverse positions 2 through 5
-    swap position 6 with position 0
-    swap letter a with letter g
-    swap position 2 with position 5
-    reverse positions 2 through 3
-    swap letter b with letter d
-    reverse positions 3 through 7
-    swap position 2 with position 5
-    swap letter d with letter b
-    reverse positions 0 through 3
-    swap letter e with letter g
-    rotate based on position of letter h
-    move position 4 to position 3
-    reverse positions 0 through 6
-    swap position 4 with position 1
-    swap position 6 with position 4
-    move position 7 to position 5
-    swap position 6 with position 4
-    reverse positions 5 through 6
-    move position 0 to position 6
-    swap position 5 with position 0
-    reverse positions 2 through 5
-    rotate right 0 steps
-    swap position 7 with position 0
-    swap position 0 with position 2
-    swap position 2 with position 5
-    swap letter h with letter c
-    rotate left 1 step
-    reverse positions 6 through 7
-    swap letter g with letter a
-    reverse positions 3 through 7
-    move position 2 to position 4
-    reverse positions 0 through 6
-    rotate based on position of letter g
-    swap position 0 with position 6
-    move position 2 to position 0
-    rotate left 3 steps
-    reverse positions 2 through 5
-    rotate based on position of letter a
-    reverse positions 1 through 4
-    move position 2 to position 3
-    rotate right 2 steps
-    rotate based on position of letter f
-    rotate based on position of letter f
-    swap letter g with letter a
-    rotate right 0 steps
-    swap letter f with letter h
-    swap letter f with letter b
-    swap letter d with letter e
-    swap position 0 with position 7
-    move position 3 to position 0
-    swap position 3 with position 0
-    rotate right 4 steps
-    rotate based on position of letter a
-    reverse positions 0 through 7
-    rotate left 6 steps
-    swap letter d with letter h
-    reverse positions 0 through 4
-    rotate based on position of letter f
-    move position 5 to position 3
-    move position 1 to position 3
-    move position 6 to position 0
-    swap letter f with letter c
-    rotate based on position of letter h
-    reverse positions 6 through 7 |}]
+    () |}]
+
+
+  
